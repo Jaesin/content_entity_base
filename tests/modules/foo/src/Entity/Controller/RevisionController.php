7d615d4a1d96375d7ce42c07bcafc620564881fc
Daniel Wehner <daniel@tag1consulting.com>
2015-10-12 16:54:11 +0200

    add revision controller

---
+++ tests/modules/foo/src/Entity/Controller/RevisionController.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\foo\Entity\Controller\RevisionController.
+ */
+
+namespace Drupal\foo\Entity\Controller;
+
+use Drupal\content_entity_base\Entity\Controller\RevisionControllerTrait;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Render\RendererInterface;
+
+class RevisionController extends ControllerBase {
+
+  use RevisionControllerTrait;
+
+  public function entityManager() {
+    // TODO: Implement entityManager() method.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function renderer() {
+    return \Drupal::service('renderer');
+  }
+
+  public function languageManager() {
+    return \Drupal::service('language_manager');
+  }
+
+  public function showRevision($revision_id) {
+  }
+
+  public function revisionPageTitle($revision_id) {
+  }
+
+  protected function hasRevertRevisionPermission(EntityInterface $entity) {
+    return $this->currentUser()->hasPermission("revert all {$entity->id()} revisions");
+  }
+
+  protected function hasDeleteRevisionPermission(EntityInterface $entity) {
+    return $this->currentUser()->hasPermission("delete all {$entity->id()} revisions");
+  }
+
+  protected function buildRevertRevisionLink(EntityInterface $entity, $revision_id) {
+  }
+
+  protected function buildDeleteRevisionLink(EntityInterface $entity, $revision_id) {
+  }
+
+  protected function getRevisionDescription(EntityInterface $revision, $is_current = FALSE) {
+  }
+
+  protected function getRevisionTitle(EntityInterface $revision) {
+  }
+
+  protected function getRevisionEntityTypeId() {
+  }
+
+  protected function getEntityViewBuilder(EntityManagerInterface $entity_manager, RendererInterface $renderer) {
+  }
+
+  public function revisionOverview(ContentEntityInterface $entity) {
+  }
+
+  protected function getOperationLinks(EntityInterface $entity, $revision_id) {
+  }
+
+}
