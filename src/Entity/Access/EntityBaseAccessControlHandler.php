e830b63ec0dd5a589aaf9b368a50e15dc85ff95b
heddn <lucashedding@1463982.no-reply.drupal.org>
2016-03-22 08:45:43 -0600

    Fixes for #58 for entity creation permissions

--- src/Entity/Access/EntityBaseAccessControlHandler.php
+++ src/Entity/Access/EntityBaseAccessControlHandler.php
@@ -23,9 +23,25 @@ class EntityBaseAccessControlHandler extends EntityAccessControlHandler {
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
     $access = parent::checkAccess($entity, $operation, $account);
 
-    if ($operation === 'view') {
+    if ($operation == 'view') {
       $access = $access->orIf(AccessResult::allowedIfHasPermission($account, 'access ' . $entity->getEntityTypeId()));
     }
+    return $access;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
+    $access = parent::checkCreateAccess($account, $context, $entity_bundle);
+    $entity_type_id = $this->entityTypeId;
+    if (!$entity_bundle) {
+      $access = $access->orIf(AccessResult::allowedIf($account->hasPermission("access $entity_type_id overview")))->cachePerPermissions();
+    }
+    else {
+      $access = $access->orIf(AccessResult::allowedIf($account->hasPermission("create $entity_bundle $entity_type_id")))->cachePerPermissions();
+    }
+
     return $access;
   }
 
